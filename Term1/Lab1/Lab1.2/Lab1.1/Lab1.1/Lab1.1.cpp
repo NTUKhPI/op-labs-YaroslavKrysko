8dfb68c016dc521311e00e3e0dd62d8978b88331
Yaroslav Krysko <116434882+YaroslavKrysko@users.noreply.github.com>
2023-09-24 13:29:24 +0200

    Add files via upload

---
+++ Term1/Lab1/Lab1.2/Lab1.1/Lab1.1/Lab1.1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    /*setlocate(LC_ALL, "ru");*/
+
+    int x;
+
+    x = sizeof(int);
+    x = sizeof(short);
+    x = sizeof(long);
+    x = sizeof(char);
+    x = sizeof(double);
+    x = sizeof(float);
+    cout << x << endl;
+}
+

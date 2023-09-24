01bceeee218f6dcae94fb86fed46ec74f4fbce31
Yaroslav Krysko <116434882+YaroslavKrysko@users.noreply.github.com>
2023-09-24 13:05:11 +0200

    Add files via upload

---
+++ Term1/Lab1/Lab1.2/Lab1.3/Lab1.3/Lab1.3.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x = 0.0;
+
+
+    while (true) {
+        cin >> x;
+        cout << pow(x,2 * 2 * 2);
+        continue;
+    }
+    return 0;
+}

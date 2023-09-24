64f6aea9a127d4dafc7b392550a2cd7754ff146d
Yaroslav Krysko <116434882+YaroslavKrysko@users.noreply.github.com>
2023-09-24 14:02:53 +0200

    Add files via upload

---
+++ Term1/Lab1/Lab1.2/Lab1.4.1/Lab1.4.1/Lab1.4.1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	double x = 2;
+	double y = 5;
+	double z ;
+	z = x;
+	 x = y;
+	 y = z;
+
+	cout << "X = " << x << endl;
+	cout << "Y = " << y << endl;
+
+
+	return 0;
+}
---
+++ Term1/Lab1/Lab1.2/Lab1.4.2/Lab1.4.2/Lab1.4.2.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int x = 2;
+	int y = 5;
+	swap(x, y);
+
+	cout << "X = " << x << endl;
+	cout << "Y = " << y << endl;
+
+
+	return 0;
+}

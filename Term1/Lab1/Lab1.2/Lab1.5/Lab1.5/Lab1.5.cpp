a69d009ab2ced144c276d637515526659bf024d9
Yaroslav Krysko <116434882+YaroslavKrysko@users.noreply.github.com>
2023-09-24 11:49:49 +0200

    Add files via upload

---
+++ Term1/Lab1/Lab1.2/Lab1.5/Lab1.5/Lab1.5.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x = 0.0;
+	double y = 0.0;
+	
+	
+
+	
+	cout << "Paste a number more than 0" << endl;
+	while (true) {
+		cin >> x;
+		y = 365 * 24 * 3600 * x;
+		if (x <= 0) {
+			cout << "I said, more than 0"<< endl;
+			continue;
+		}
+		else {
+			cout << "In this year "<< y <<" seconds" << endl;
+		}
+	}
+	return 0;
+}
+
